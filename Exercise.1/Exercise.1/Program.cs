571696aad963875180c19b5bcd0d080bba1c4277
Zane-2244 <49199705+Zane-2244@users.noreply.github.com>
2019-04-15 15:17:29 +0200

    Add files via upload

---
+++ Exercise.1/Exercise.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace Exercise._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string  question = "Enter Number between 1 to 10:";
+            Console.WriteLine(question);
+            int number = 5;
+            if (number > 0 && number < 11)
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("invalid");
+            }
+
+
+        }
+    }
+}
